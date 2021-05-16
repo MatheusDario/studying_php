23d292336fadd63ba5cdc00bff85651ac7b8a80a
Matheus Dario <dario.matheus97@gmail.com>
2021-05-16 10:04:11 -0300

    first commit

---
+++ echo_print.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Curso PHP</title>
+    </head>
+
+    <body>
+
+        <?php 
+            //comentário de uma linha
+            echo 'Comando echo<hr/> <br/> ';
+            #comentário de uma linha
+            print '<h2>Comando print<h2/>';
+            /* comentário
+            de
+            bloco 
+            */
+        ?>
+
+       
+    </body>
+</html>
---
+++ variaveis.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Curso PHP</title>
+    </head>
+
+    <body>
+
+        <?php 
+            //string
+            $nome = "Matheus Dario";
+
+            //int
+            $idade = 23;
+
+            //float
+            $peso = 95.50;
+
+            //boolean
+            $fumante_sn = false;
+        ?>
+        <h1>Ficha Cadastral</h1>
+        <br/>
+        <p>Nome: <?=$nome ?></p>
+        <p>Idade: <?=$idade ?></p>
+        <p>Peso: <?=$peso ?></p>
+        <p>Fumante: <?=$fumante_sn ?></p>
+       
+    </body>
+</html>
