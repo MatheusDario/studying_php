f6a0d059f995901d8d3d16c023a3605dbdafdc27
Matheus Dario <dario.matheus97@gmail.com>
2021-05-17 17:44:47 -0300

    operadores-ifelse-php

--- variaveis_constantes.php
+++ variaveis_constantes.php
@@ -14,9 +14,48 @@
            } else {
                echo 'Falso';
            }
+
+           $usuario_possui_cartao_loja = true;
+           $valor_compra = 110;
+           $valor_frete = 50;
+           $recebeu_desconto_frete = false;
+
+           if($usuario_possui_cartao_loja == true && $valor_compra >= 400) {
+               $valor_frete = 0;
+               $recebeu_desconto_frete = true;
+           }else if($usuario_possui_cartao_loja == true && $valor_compra >= 300) {
+                $valor_frete = 10;
+                $recebeu_desconto_frete = true;
+           }else if($usuario_possui_cartao_loja == true && $valor_compra >= 100) {
+                $valor_frete = 25;
+                $recebeu_desconto_frete = true;
+           }
         ?>
 
-        
+        <h1>Detalhes do Pedido</h1>
+        <P>Possui cartão da loja?
+           <?php 
+                if($usuario_possui_cartao_loja == true) {
+                    echo 'SIM';
+                } else {
+                    echo 'NÃO';
+                }
+           ?>
+        </p>
+
+        <p>Valor da compra: <?= $valor_compra ?> </p>
+
+        <P>Recebeu desconto no frete?
+           <?php 
+                if($recebeu_desconto_frete == true) {
+                    echo 'SIM';
+                } else {
+                    echo 'NÃO';
+                }
+           ?>
+        </p>
+
+        <p>Valor do frete: <?= $valor_frete ?> </p>
        
     </body>
 </html>
