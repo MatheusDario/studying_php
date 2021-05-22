120b45c27b92484a672d7178fd9690f596270ee5
Matheus Dario <dario.matheus97@gmail.com>
2021-05-22 12:59:10 -0300

    sobre-include-require-funciona

---
+++ incorporando_scripts/inicio.php
@@ -0,0 +1,18 @@
+<?php 
+    include_once("menu.php"); //warning ou seja o processamento não é afetado
+
+    //require("menu.php2"); // fatal error interrompe completamente o script
+    /*
+        Diferença entre include e include_once é que se for identificado outro include_once
+        ira ser executado apenas uma vez o mesmo serve para require e require_once.
+    */
+?>
+
+conteúdo da página (início)
+<br/>
+
+<?php 
+    include_once("menu.php"); //warning ou seja o processamento não é afetado
+
+    //require("menu.php2"); // fatal error interrompe completamente o script
+?>
