74f4b6c1eb7a10d066c5a0b35608ed6a4bea52d9
Matheus Dario <dario.matheus97@gmail.com>
2021-06-14 17:57:54 -0300

    instrucoes-exec

--- php_com_pdo/index.php
+++ php_com_pdo/index.php
@@ -4,5 +4,32 @@
     $usuario = 'root';
     $senha = '';
 
-    $conexao = new PDO($dsn, $usuario , $senha);
+    //tratando exptions 
+    try {
+        $conexao = new PDO($dsn, $usuario , $senha);
+
+        //Query DDL - que irá definir a estrutura dos dados
+        $query = '
+            create table if not exists tb_usuarios(
+                id int not null primary key auto_increment,
+                nome varchar(50) not null,
+                email varchar(100) not null,
+                senha varchar(32) not null
+            )
+        ';
+
+        $retorno = $conexao->exec($query);
+        echo $retorno;
+
+        $query = '
+            delete from tb_usuarios
+        ';
+
+        $retorno = $conexao->exec($query);
+        echo $retorno;
+
+    } catch(PDOException $e) {
+        echo 'Erro: '.$e->getCode(). ' Mensagem: ' .$e->getMessage();
+    }
+   
 ?>
