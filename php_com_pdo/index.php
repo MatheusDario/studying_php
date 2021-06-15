94ffd29aef41824b714a869df09535b2c2961748
Matheus Dario <dario.matheus97@gmail.com>
2021-06-14 21:17:23 -0300

    utilizando-foreach

--- php_com_pdo/index.php
+++ php_com_pdo/index.php
@@ -9,16 +9,26 @@
         $conexao = new PDO($dsn, $usuario , $senha);
         
         $query = '
-            select * from tb_usuarios
+            select * from tb_usuarios 
         ';
         
-        $stmt = $conexao->query($query);
-        $lista = $stmt->fetchAll(PDO::FETCH_OBJ); //Indices associativos ou FETCH_NUM 
-        echo '<pre>';
-        print_r($lista);
-        echo '</pre>';
-
-        echo $lista[0]->nome;
+        //$stmt = $conexao->query($query);
+        foreach($conexao->query($query) as $key => $value) {
+            print_r($value['nome']);
+            echo '<hr>';
+        }
+        
+        //$lista_usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC); //Indices associativos/ FETCH_NUM/ FETCH_OBJ Indices objetos 
+        
+        //echo '<pre>';
+        //print_r($lista_usuarios);
+        //echo '</pre>';
+        /*
+        foreach($lista_usuarios as $key => $value) {
+            echo($value['nome']);
+            echo '<hr>';
+        }
+        */
 
     } catch(PDOException $e) {
         echo 'Erro: '.$e->getCode(). ' Mensagem: ' .$e->getMessage();
