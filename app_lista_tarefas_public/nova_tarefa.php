bd080ee97d57f180130a0ecbe5be847f812e83f9
Matheus Dario <dario.matheus97@gmail.com>
2021-06-15 13:47:18 -0300

    inserindo-registros

--- app_lista_tarefas_public/nova_tarefa.php
+++ app_lista_tarefas_public/nova_tarefa.php
@@ -19,6 +19,13 @@
 			</div>
 		</nav>
 
+		<!--Lógica Mensagem -->
+		<? if(isset($_GET['inclusao']) && $_GET['inclusao'] == 1) { ?>   
+			<div class="bg-success pt-2 text-white d-flex justify-content-center">
+				<h5>Tarefa inserida com Sucesso!</h5> 
+			</div>
+		<? } ?>
+
 		<div class="container app">
 			<div class="row">
 				<div class="col-md-3 menu">
