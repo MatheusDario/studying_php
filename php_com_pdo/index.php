9db8cc415c4a0fc5eb6106027e027e1a4ccad727
Matheus Dario <dario.matheus97@gmail.com>
2021-06-14 22:07:09 -0300

    prepare-sql

--- php_com_pdo/index.php
+++ php_com_pdo/index.php
@@ -1,37 +1,54 @@
 <?php
 
-    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
-    $usuario = 'root';
-    $senha = '';
+    //if(!empty($_POST['usuario']) &&  !empty($_POST['senha'])) {
 
-    //tratando exptions 
-    try {
-        $conexao = new PDO($dsn, $usuario , $senha);
-        
-        $query = '
-            select * from tb_usuarios 
-        ';
-        
-        //$stmt = $conexao->query($query);
-        foreach($conexao->query($query) as $key => $value) {
-            print_r($value['nome']);
-            echo '<hr>';
-        }
-        
-        //$lista_usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC); //Indices associativos/ FETCH_NUM/ FETCH_OBJ Indices objetos 
-        
-        //echo '<pre>';
-        //print_r($lista_usuarios);
-        //echo '</pre>';
-        /*
-        foreach($lista_usuarios as $key => $value) {
-            echo($value['nome']);
-            echo '<hr>';
-        }
-        */
+        $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+        $usuario = 'root';
+        $senha = '';
+    
+        //tratando exptions 
+        try {
+            $conexao = new PDO($dsn, $usuario , $senha); 
 
-    } catch(PDOException $e) {
-        echo 'Erro: '.$e->getCode(). ' Mensagem: ' .$e->getMessage();
-    }
+            $query = "select * from tb_usuarios where ";
+            $query .= " email = :usuario ";
+            $query .= " AND senha = :senha ";
+            
+            $stmt = $conexao->prepare($query);
+
+            $stmt->bindValue(':usuario', $_POST['usuario']);
+            $stmt->bindValue(':senha', $_POST['senha']);
+
+            $stmt->execute();
+
+            $usuario = $stmt->fetch();
+
+            echo '<pre>';
+            print_r($usuario);
+            echo '</pre>';
+
+        } catch(PDOException $e) {
+            echo 'Erro: '.$e->getCode(). ' Mensagem: ' .$e->getMessage();
+        }
+
+    //} 
+
    
 ?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+    </head>
+
+    <body>
+        <form method="post" action="index.php"> <!--O método de envio e para aonde vai-->
+            <input type="text" placeholder="usuário" name="usuario" />
+            <br/>
+            <input type="password" placeholder="senha" name="senha" />
+            <br/>
+            <button type="submit">Login</button>
+        </form>
+    </body>
+</html>
